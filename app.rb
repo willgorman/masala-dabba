7eb19d664d7e48ebe0f170c3f042ba6675baf377
Will Gorman <will.gorman@gmail.com>
2012-11-21 15:09:09 -0500

    Testing Pony

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+require 'pony'
 $stdout.sync = true
+
+Pony.options = {
+  :via => :smtp,
+  :via_options => {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :domain => 'heroku.com',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+}
 
 get '/' do
   "Hello World!"
@@ -9,6 +23,11 @@ post '/parse' do
   puts params[:from]
   puts params[:to]
   puts params[:subject]
-  puts params[:text]
   puts params[:html]
+  Pony.mail({
+    :to => ENV['TEST_EMAIL'],
+    :from => 'dabba',
+    :subject => ENV['RTM_LIST'],
+    :body => 'TESTING'
+    })
 end
